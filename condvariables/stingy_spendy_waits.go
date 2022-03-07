e4c6e74f6448e1da461ef34a5f70862233cfb8ac
Vib-UX <vibchess123@gmail.com>
2022-03-07 21:30:47 +0530

    Stingy-Spendy ConditionVariable

---
+++ condvariables/stingy_spendy_waits.go
@@ -0,0 +1,52 @@
+/*
+	Consdition Variables are great way of thread synchronization which kind of leverages the idea
+	of using a conditions to signal and wait until the conditions become true inorder to
+	execute multiple threads
+*/
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	money          = 100
+	lock           = sync.Mutex{}
+	moneyDeposited = sync.NewCond(&lock)
+)
+
+func stingy() {
+	for i := 1; i <= 1000; i++ {
+		lock.Lock()
+		money += 10
+		fmt.Println("Stingy sees balance of ", money)
+		moneyDeposited.Signal()
+		lock.Unlock()
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("Stingy Done")
+}
+
+func spendy() {
+	for i := 1; i <= 1000; i++ {
+		lock.Lock()
+		for money-20 < 0 {
+			moneyDeposited.Wait()
+		}
+		money -= 20
+		fmt.Println("Spendy sees balance of ", money)
+		lock.Unlock()
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("Spendy Done")
+}
+
+func main() {
+	go stingy()
+	go spendy()
+	time.Sleep(3000 * time.Millisecond)
+	print(money)
+}
